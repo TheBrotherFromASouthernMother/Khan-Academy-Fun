f1552acabd31be2cab1fa9745f7f555922b1544a
Christian Lowe <christglowe@gmail.com>
2017-06-30 00:25:54 -0500

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,54 +1,64 @@
+
+//This is a 2D array containing a question answer bank for this simple quiz.
 var testQuestions = [
   ["What country is named after the Equator?", "ecuador"],
   ["The initials EU stand for what supranational organization?", "european union"],
   ["Seoul is the capital of what country?", "south korea"]
 ];
 
-var TrueAnswers = [];
-var FalseAnsers = [];
+//Empty array literal to carry questions that have been answered correctly
+var CorrectAnswers = [];
+
+//Empty array literal to carry questions that have been answered incorrectly
+var IncorrectAnwsers = [];
+
+//Tracks the user's responses to the answers
 var response;
+
+//Empty html div on which the quiz questions will be outputted once the quiz is over
 var output = document.getElementById("output");
 
+//Function declaration so I don't have to write doc.wrt every damn time.
 function print(message) {
-  
   document.write(message);
 }
 
 
-
+//Iterates through the 2D array above and displays quiz questions through JavaScript prompts
 for (var i = 0; i < testQuestions.length; i++) {
 
   response = prompt(testQuestions[i][0]);
   
+  //protects against case errors 
   response = response.toLowerCase();
   if (response == testQuestions[i][1] ) {
-    
-      TrueAnswers.push(testQuestions[i][0]);
+    //adds correct answers to corresponding array
+      CorrectAnswers.push(testQuestions[i][0]);
   
   } else {
-  
-    FalseAnsers.push(testQuestions[i][0]);
+  //vice versa: adds incorrect answers to correspond array
+    IncorrectAnwsers.push(testQuestions[i][0]);
   }
 
 }
-
-if (TrueAnswers.length > 0) {
-    
-    print("You got (" + TrueAnswers.length + ") questions right: <br>")
+//checks whether users got any answers correct
+if (CorrectAnswers.length > 0) {
+    //prints the numers of corrects and anwsers and their corresponding questions to the document
+    print("You got (" + CorrectAnswers.length + ") questions right: <br>")
     print("<ol>");
-    for (var j = 0; j < TrueAnswers.length; j ++) {
-    print("<li>" + TrueAnswers[j] + "</li>");
+    for (var j = 0; j < CorrectAnswers.length; j ++) {
+    print("<li>" + CorrectAnswers[j] + "</li>");
     }
     print("</ol>");
 }
 
-
-if (FalseAnsers.length > 0) {
+//checks to see if user got any incorrects
+if (IncorrectAnwsers.length > 0) {
       
-    print("You got (" + FalseAnsers.length +  ") these questions wrong: <br>");
+    print("You got (" + IncorrectAnwsers.length +  ") these questions wrong: <br>");
     print("<ol>");
-    for (var j = 0; j < FalseAnsers.length; j ++) {
-      print("<li>" + FalseAnsers[j] + "</li>");
+    for (var j = 0; j < IncorrectAnwsers.length; j ++) {
+      print("<li>" + IncorrectAnwsers[j] + "</li>");
     }
     print("</ol>");
 }
